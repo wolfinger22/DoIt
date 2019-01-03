6e2e887ef059a879d769d25bdbd868908e471f53
Wolf Wittmund <>
2019-01-03 23:11:59 +0100

    2 screens

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Wolf Wittmund on 2019-01-03.
+//  Copyright © 2019 Wolf Wittmund. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
